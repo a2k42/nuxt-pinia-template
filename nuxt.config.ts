9f5686d4454732cc0305659a23142683cf2fe8cc
a2k42 <1123245+a2k42@users.noreply.github.com>
2024-05-15 15:06:30 +0100

    Added title and dev port to config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: "Nuxt Pinia Template"
+        }
+    },
     css: ["~/assets/css/main.css"],
+    devServer: {
+        port: +(process.env.PORT || 3000),
+    },
     devtools: { enabled: true },
+    telemetry: false,
 });
