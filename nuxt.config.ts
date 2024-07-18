f28e89d25f02ee2c7b0d3462a919907640c33d28
a2k42 <1123245+a2k42@users.noreply.github.com>
2024-07-18 12:29:59 +0100

    Fix - added baseURL to app config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     app: {
+        baseURL: "/nuxt-pinia-template",
         head: {
             title: "Nuxt Pinia Template"
         },
