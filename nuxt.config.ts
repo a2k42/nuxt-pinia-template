9399cc1e7620af43babcfddc71425242e41fc5de
a2k42 <1123245+a2k42@users.noreply.github.com>
2024-11-20 16:37:41 +0000

    Update versions and move into app subfolder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,17 +10,29 @@ export default defineNuxtConfig({
                     href: "nuxt-pinia-template/favicon.ico",
                 },
             ],
+            htmlAttrs: {
+                style: "background-color: #111; color: #ddd",
+            },
             title: "Nuxt Pinia Template",
         },
         layoutTransition: { name: "layout", mode: "out-in" },
         pageTransition: { name: "layout", mode: "out-in" },
     },
+
     css: ["~/assets/css/main.css"],
+
     devServer: {
         port: +(process.env.PORT || 3000),
     },
+
     devtools: { enabled: false },
+
+    future: {
+        compatibilityVersion: 4,
+    },
+
     modules: ["@pinia/nuxt", "@vueuse/nuxt"],
     ssr: false,
     telemetry: false,
+    compatibilityDate: "2024-07-19",
 });
